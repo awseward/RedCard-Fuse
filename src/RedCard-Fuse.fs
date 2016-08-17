78bfe86d9dcef66bfdc7ac357f0e64735cad4512
Andrew William Seward <awswrd@gmail.com>
2016-08-17 13:37:36 -0700

    Organize things a bit
    Ready to actually fetch data, I believe

--- src/RedCard-Fuse.fs
+++ src/RedCard-Fuse.fs
@@ -4,8 +4,11 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fuse
 open Fable.Import.Fetch
+open Fable.Helpers.Fetch
 
 module RedCardFuse =
+  (* Type stuff *)
+
   type Player =
     {
       id: int
@@ -31,38 +34,19 @@ module RedCardFuse =
         position = (parsePosition player.position)
     }
 
-  let playersJson = """[{"id":1,"name":"Bro Duderson","position":"F","yellowCards":10,"redCards":2,"team":"UCSB","league":"NCAA","country":"USA"}]"""
+  (* Data stuff *)
+  let playersJson = """[{"id":1,"name":"Person McPersonFace","position":"F","yellowCards":10,"redCards":2,"team":"UCSB","league":"NCAA","country":"USA"}]"""
+  let parsedPlayers = ofJson<Player[]>(playersJson)
 
-  let parsedPlayers =
-    ofJson<Player[]>(playersJson)
-    |> Seq.map fixPosition
+  let fetchedPlayers = [] // TODO
 
-  printfn "%A" parsedPlayers
-
-  let DefaultPlayer =
-    {
-      id = 1
-      name = "Person McPersonface"
-      position = parsePosition "M"
-      yellowCards = 10
-      redCards = 2
-      team = "UCSB"
-      league = "NCAA"
-      country = "USA"
-    }
-
+  (* Set up Observables the App actually will look at *)
   let players = Observable.create()
 
-  players.add DefaultPlayer
-  players.add
-    { DefaultPlayer with
-        id = 2
-        name = "Jane Doe"
-        position = parsePosition "F"
-    }
-  players.add
-    { DefaultPlayer with
-        id = 2
-        name = "Donny Nullerson"
-        position = parsePosition "bad_data"
-    }
+  parsedPlayers
+  |> Seq.map fixPosition
+  |> Seq.iter (fun p -> players.add p)
+
+  fetchedPlayers
+  |> Seq.map fixPosition
+  |> Seq.iter (fun p -> players.add p)
