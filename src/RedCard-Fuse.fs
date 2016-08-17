38c7cb9e406f192ac8976ab03c8a3faf6d912817
Andrew William Seward <awswrd@gmail.com>
2016-08-17 12:53:25 -0700

    Trying to use `ofJson` from fable

--- src/RedCard-Fuse.fs
+++ src/RedCard-Fuse.fs
@@ -1,6 +1,7 @@
 namespace App
 
 open Fable.Core
+open Fable.Core.JsInterop
 open Fuse
 open Fable.Import.Fetch
 
@@ -24,6 +25,10 @@ module RedCardFuse =
     | "D" -> "Defender"
     | "G" -> "Goaltender"
     | _   -> "Bench"
+
+  (* let playerJson = """[{"id":1,"name":"Bro Duderson","position":"F","yellowCards":10,"redCards":2,"team":"UCSB","league":"NCAA","country":"USA"}]""" *)
+
+  (* let parsedPlayer = ofJson<Player>(playerJson) *)
 
   let DefaultPlayer =
     {
